b964978f3485863830477d5c8d60f12c505cd879
awallenfang <marvin@wallenfang.de>
2025-07-16 21:07:36 +0200

    Better shader hash function

--- src/ui/background.rs
+++ src/ui/background.rs
@@ -1,7 +1,11 @@
-use std::time;
+use std::{sync::Arc, time};
 
-use crate::ui::{shaders::{self, make_effect}, DelaxEvent};
+use crate::ui::{
+    DelaxEvent, InputData,
+    shaders::{self, make_effect},
+};
 use nih_plug::{nih_dbg, params::Param, prelude::*};
+use shaders::TIME;
 use vizia_plug::{
     vizia::{
         prelude::*,
@@ -12,15 +16,12 @@ use vizia_plug::{
     },
     widgets::param_base::ParamWidgetBase,
 };
-use shaders::TIME;
 
 const SHADER: &'static str = include_str!("shaders/background.sksl");
 
 /// A switch to control a boolean nih-plug parameter
 pub struct Background {
-    time: f32,
-    // shader: Option<Shader>,
-    effect: Option<RuntimeEffect>
+    effect: Option<RuntimeEffect>,
 }
 
 impl Background {
@@ -34,7 +35,8 @@ impl Background {
         } else if let Err(error) = result {
             nih_dbg!(error);
         }
-        Self { time: 0., /*shader,*/ effect }.build(cx, |_| {})
+
+        Self { effect }.build(cx, |_| {})
     }
 }
 
@@ -45,26 +47,29 @@ impl View for Background {
 
     fn draw(&self, cx: &mut DrawContext, canvas: &Canvas) {
         let bounds = cx.bounds();
+
         let mut path = Path::new();
         let rect = Rect::new(bounds.x, bounds.y, bounds.x + bounds.w, bounds.y + bounds.h);
         path.add_rect(rect, None);
+
         let mut paint = Paint::default();
         paint.set_style(PaintStyle::Fill);
 
         if let Some(effect) = &self.effect {
             let mut shader_to_device = Matrix::translate((bounds.x, bounds.y));
-            shader_to_device = *shader_to_device.pre_scale((bounds.w, bounds.h), None);
+            let local_matrix = *shader_to_device.pre_scale((bounds.w, bounds.h), None);
 
-            let local_matrix = shader_to_device;
             let mut builder = RuntimeShaderBuilder::new(effect.clone());
-            let _ = builder.set_uniform_float("time", &[TIME.load(std::sync::atomic::Ordering::Relaxed)]);
+            let _ = builder
+                .set_uniform_float("time", &[TIME.load(std::sync::atomic::Ordering::Relaxed)]);
+            
             let shader = builder.make_shader(&local_matrix);
             if let Some(s) = shader {
                 paint.set_shader(s);
             }
         }
+
         canvas.draw_path(&path, &paint);
         cx.needs_redraw();
     }
-
 }
