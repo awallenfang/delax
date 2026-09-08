9efa3b80d50d415257784b0afaddd7958e8b8224
awallenfang <marvin@wallenfang.de>
2026-09-08 18:02:31 +0200

    Shader fixes

--- src/slint_ui/uniforms.rs
+++ src/slint_ui/uniforms.rs
@@ -1,3 +1,4 @@
+use std::marker::PhantomData;
 use bytemuck::{Pod, Zeroable};
 
 #[repr(C)]
@@ -9,9 +10,10 @@ pub struct SpectrumUniforms {
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Pod, Zeroable)]
 pub struct BufferUniforms {
-    pub levels_dry: [f32; 128],
-    pub levels_wet: [f32; 128],
+    pub levels_dry: [[f32; 4]; 16],
+    pub levels_wet: [[f32; 4]; 16],
     pub primary_col: [f32; 4],
     pub secondary_col: [f32; 4],
+    pub params: [f32; 4],
 }
 
