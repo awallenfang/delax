52d263e13aa754a0ee7f9e8492cda882bed5da1b
awallenfang <marvin@wallenfang.de>
2026-09-12 12:33:12 +0200

    Better input filtering

--- src/filters/params.rs
+++ src/filters/params.rs
@@ -51,18 +51,14 @@ pub struct SVFParams {
     pub svf_mix_l: FloatParam,
     #[id = "svf_mix_r"]
     pub svf_mix_r: FloatParam,
-    #[id = "input_svf_cutoff_l"]
-    pub input_svf_cutoff_l: FloatParam,
-    #[id = "input_svf_cutoff_r"]
-    pub input_svf_cutoff_r: FloatParam,
-    #[id = "input_svf_res_l"]
-    pub input_svf_res_l: FloatParam,
-    #[id = "input_svf_res_r"]
-    pub input_svf_res_r: FloatParam,
-    #[id = "input_svf_filter_mode_l"]
-    pub input_svf_filter_mode_l: EnumParam<SVFFilterMode>,
-    #[id = "input_svf_filter_mode_r"]
-    pub input_svf_filter_mode_r: EnumParam<SVFFilterMode>,
+    #[id = "input_svf_cutoff_low_l"]
+    pub input_svf_cutoff_low_l: FloatParam,
+    #[id = "input_svf_cutoff_high_l"]
+    pub input_svf_cutoff_high_l: FloatParam,
+    #[id = "input_svf_cutoff_low_r"]
+    pub input_svf_cutoff_low_r: FloatParam,
+    #[id = "input_svf_cutoff_high_r"]
+    pub input_svf_cutoff_high_r: FloatParam,
 }
 
 impl Default for SVFParams {
@@ -115,9 +111,9 @@ impl Default for SVFParams {
             )
             .with_smoother(SmoothingStyle::Linear(50.))
             .with_value_to_string(formatters::v2s_f32_rounded(2)),
-            input_svf_cutoff_l: FloatParam::new(
-                "SVF Cutoff",
-                500.,
+            input_svf_cutoff_low_l: FloatParam::new(
+                "SVF Cutoff Low L",
+                20000.,
                 FloatRange::Skewed {
                     min: 0.,
                     max: 20000.,
@@ -127,9 +123,9 @@ impl Default for SVFParams {
             .with_smoother(SmoothingStyle::Linear(50.))
             .with_value_to_string(formatters::v2s_f32_hz_then_khz(2))
             .with_string_to_value(formatters::s2v_f32_hz_then_khz()),
-            input_svf_cutoff_r: FloatParam::new(
-                "SVF Cutoff Channel 2",
-                500.,
+            input_svf_cutoff_high_l: FloatParam::new(
+                "SVF Cutoff High L",
+                0.,
                 FloatRange::Skewed {
                     min: 0.,
                     max: 20000.,
@@ -139,25 +135,30 @@ impl Default for SVFParams {
             .with_smoother(SmoothingStyle::Linear(50.))
             .with_value_to_string(formatters::v2s_f32_hz_then_khz(2))
             .with_string_to_value(formatters::s2v_f32_hz_then_khz()),
-            input_svf_res_l: FloatParam::new(
-                "SVF Res",
-                0.2,
-                FloatRange::Linear { min: 0., max: 1. },
+            input_svf_cutoff_high_r: FloatParam::new(
+                "SVF Cutoff High R",
+                0.,
+                FloatRange::Skewed {
+                    min: 0.,
+                    max: 20000.,
+                    factor: 0.5,
+                },
             )
             .with_smoother(SmoothingStyle::Linear(50.))
-            .with_value_to_string(formatters::v2s_f32_rounded(2)),
-            input_svf_res_r: FloatParam::new(
-                "SVF Res Channel 2",
-                0.2,
-                FloatRange::Linear { min: 0., max: 1. },
+            .with_value_to_string(formatters::v2s_f32_hz_then_khz(2))
+            .with_string_to_value(formatters::s2v_f32_hz_then_khz()),
+            input_svf_cutoff_low_r: FloatParam::new(
+                "SVF Cutoff Low R",
+                20000.,
+                FloatRange::Skewed {
+                    min: 0.,
+                    max: 20000.,
+                    factor: 0.5,
+                },
             )
             .with_smoother(SmoothingStyle::Linear(50.))
-            .with_value_to_string(formatters::v2s_f32_rounded(2)),
-            input_svf_filter_mode_l: EnumParam::new("SVF Filter Mode", SVFFilterMode::Low),
-            input_svf_filter_mode_r: EnumParam::new(
-                "SVF Filter Mode Channel 2",
-                SVFFilterMode::Low,
-            ),
+            .with_value_to_string(formatters::v2s_f32_hz_then_khz(2))
+            .with_string_to_value(formatters::s2v_f32_hz_then_khz()),
         }
     }
 }
